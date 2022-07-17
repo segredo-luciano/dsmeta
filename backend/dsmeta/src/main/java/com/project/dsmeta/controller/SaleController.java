dcd80f1da35d949d6ebbdbc57bfb2829e427897c
Luciano Segredo <luciano.junior@metareports.com.br>
2022-07-17 20:48:46 -0300

    twilio sms

--- backend/dsmeta/src/main/java/com/project/dsmeta/controller/SaleController.java
+++ backend/dsmeta/src/main/java/com/project/dsmeta/controller/SaleController.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.dsmeta.entities.Sale;
 import com.project.dsmeta.services.SaleService;
+import com.project.dsmeta.services.SmsService;
 
 @RequestMapping(value="/sales")
 @RestController
@@ -18,10 +20,18 @@ public class SaleController {
 	@Autowired
 	private SaleService saleService;
 	
+	@Autowired
+	private SmsService smsService;
+	
 	@GetMapping("listAll")
 	public Page<Sale> findSales(@RequestParam(value="minDate", defaultValue="") String minDate, 
 							@RequestParam(value="maxDate", defaultValue="") String maxDate, 
 							Pageable pageable) {
 		return saleService.findSales(minDate, maxDate, pageable);
+	}
+	
+	@GetMapping("/{id}/sms")
+	public void sendSms(@PathVariable Long id) {
+		smsService.sendSms(id);
 	}
 }
